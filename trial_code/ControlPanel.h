42122a45ad453095811a0286020c4a645329f7fb
hydhwbad <62399296+hydhwbad@users.noreply.github.com>
2020-03-27 15:59:34 +0530

    Add files via upload

---
+++ trial_code/ControlPanel.h
@@ -0,0 +1,17 @@
+#ifndef ControlPanel_H
+#define ControlPanel_H
+
+#include "Arduino.h"
+
+class ControlPanel
+{
+   private:
+
+   public:   
+   ControlPanel(void);
+
+   float getParameter(int p);
+  
+};
+#endif
+
---
+++ trial_code/params.h
@@ -0,0 +1,5 @@
+#define RR_PER_MIN 					1
+#define TIDAL_VOLUME_ML				2
+#define PATIENT_MAX_PRESSURE_MBAR	3
+#define FIO2_FLOAT_PERCENTAGE		4
+		
